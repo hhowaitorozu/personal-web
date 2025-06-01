3046b7bba6e23a6348ec61f0bb69105e9569c2b7
hhowaitorozu <nrinsalwa@gmail.com>
2025-06-01 13:54:14 +0700

    init repo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#element", {
+  strings: ["Computer Science Student.", "Cloud Enthusiast.", "Full-time Enjoyer."],
+  loop: true,
+  typeSpeed: 70,
+  backSpeed: 70,
+});
